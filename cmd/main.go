43824ee5d5a415d505997d28c5cf7893d5d5be34
Harold <harold.vilander@gmail.com>
2024-07-06 06:12:42 -0500

    hot garbage

--- cmd/main.go
+++ cmd/main.go
@@ -3,7 +3,12 @@ package main
 import (
   "html/template"
   "io"
+  //"database/sql"
+  //"fmt"
+  //"os"
 
+  //"github.com/joho/godotenv"
+  //"github.com/lib/pq"
   "github.com/labstack/echo/v4"
   "github.com/labstack/echo/v4/middleware"
 )
@@ -26,20 +31,75 @@ type Count struct {
   Count int
 }
 
+type Contact struct {
+  Name string
+  Email string
+}
+
+func newContact(name, email string) Contact {
+  return Contact{
+    Name: name,
+    Email: email,
+  }
+}
+
+type Contacts = []Contact
+
+type Data struct {
+  Contacts Contacts
+}
+
+func newData() Data {
+  return Data{
+    Contacts: []Contact{
+      newContact("John", "jd@gmail.com"),
+      newContact("Clara", "cd@gmail.com"),
+    },
+  }
+}
+
+
 func main() {
+  /*
+  // Setup connection to postgresSQL db
+  dbConnectionString := os.Getenv("DB_CONN_STR")
+  db, err := sql.Open("postgres", dbConnectionString)
+
+  if err != nil {
+    panic(err)
+  }
+
+  defer db.Close()
+  var version string
+  
+  if err := db.QueryRow("select version()").Scan(&version); err != nil {
+    panic(err)
+  }
+
+  // Print db version
+  fmt.Printf("version=%s\n", version)
+  */
+
+
+
+  // Start the server
   e := echo.New()
   e.Use(middleware.Logger())
 
-  count := Count { Count: 0 }
+  data := newData()
   e.Renderer = newTemplate()
 
+  // handlers TODO move these to their own files at some point
   e.GET("/", func(c echo.Context) error {
-    return c.Render(200, "index", count)
+    return c.Render(200, "index", "test")
   })
 
-  e.POST("/count", func(c echo.Context) error {
-    count.Count++
-    return c.Render(200, "count", count)
+  e.POST("/contacts", func(c echo.Context) error {
+    name := c.FormValue("name")
+    email:= c.FormValue("email")
+    data.Contacts = append(data.Contacts, newContact(name, email))
+
+    return c.Render(200, "index", data)
   })
 
   e.Logger.Fatal(e.Start(":42069"))
--- go.mod
+++ go.mod
@@ -2,7 +2,11 @@ module mywebsite.tv/name
 
 go 1.22.5
 
-require github.com/labstack/echo/v4 v4.12.0
+require (
+	github.com/joho/godotenv v1.5.1
+	github.com/labstack/echo/v4 v4.12.0
+	github.com/lib/pq v1.10.9
+)
 
 require (
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
